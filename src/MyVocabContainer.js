ead6fcfda03c516f689bac9eadefd426243f3bd7
hilbert-spaess <jacobcoxon@hotmail.co.uk>
2020-09-16 14:01:07 +0000

    hi

--- src/MyVocabContainer.js
+++ src/MyVocabContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Card, Button, Row, Col, Container, CardDeck, ProgressBar} from 'react-bootstrap';
+import {Card, Button, Row, Col, Container, CardDeck, ProgressBar, Modal} from 'react-bootstrap';
 import {loadVocab} from './client.js';
 import {useApi} from './use-api.js';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -27,14 +27,19 @@ function shuffle(array) {
 }
 
 export class MyVocabContainer extends React.Component {
+    
+    state = {
+        showDialog: false
+    }
+    
     render () {
         return (
-            <BarWrapped WrappedComponent={MyVocabContainer1}/>
+            <BarWrapped WrappedComponent={LoadingVocabContainer}/>
         );
     }
 }
 
-export const MyVocabContainer1 = () => {
+export const LoadingVocabContainer = () => {
     
      const {login, getAccessTokenWithPopup } = useAuth0();
      const opts = {audience: APIHOST};
@@ -56,40 +61,91 @@ export const MyVocabContainer1 = () => {
     }
     
     return (
-         <div>
+         <MyVocabContainer1
+        data={data}/>
+        );
+}   
+
+class MyVocabContainer1 extends React.Component {
+    
+    state = {
+        showDialog: false,
+        detailId: null,
+        detailActive: null
+    }
+    
+    showDetail = (id, active) => {
+        
+        console.log(id);
+        
+        this.setState({detailId: id,
+                       detailActive: active,
+                       showDialog: true
+                      })
+    }
+    
+    onHide = () => {
+        
+        this.setState({showDialog: false})
+        
+    }
+    
+    render () {
+        
+        return (
+            <div>
+            <Modal centered show={this.state.showDialog} onHide={this.onHide}>
+            {this.state.detailActive && this.props.data.active[this.state.detailId]['w']}
+    </Modal>
         <div
-            style={{textAlign: "center", fontSize: "4rem", marginTop: "3rem"}}>
+        style={{textAlign: "center", fontSize: "4rem", marginTop: "3rem"}}>
                 Active Vocab <br></br>
 </div>
                 <VocabGrid
-                VocabDict = {data.active}
-                size="3em"
+                VocabDict = {this.props.data.active}
+                showDetail={this.showDetail}
+                size="2em"
                 active={true}/>
                 <br></br>
 <div style={{textAlign: "center", fontSize: "4rem", marginTop: "3rem"}}>
                 Future Vocab: <br></br>
 </div>
                 <VocabGrid
-                VocabDict = {data.future}
+                VocabDict = {this.props.data.future}
                 size="2em"
                 active={false}/>
                     </div>
-        );
-}        
+    );
+
+    }
+}
 
 class VocabGrid extends React.Component {
+    
+    state = {
+        keys: null
+    }
+    
+    componentDidMount = () => {
+        var keys = Object.keys(this.props.VocabDict);
+        shuffle(keys);
+        this.state.keys = keys
+    }
+        
     
     render () {
         
         var vocabCards = [];
-        var keys = Object.keys(this.props.VocabDict);
+         var keys = Object.keys(this.props.VocabDict);
         shuffle(keys);
         for (var i = 0; i < keys.length; i++) {
             
-            vocabCards.push(<VocabCard 
+            vocabCards.push(<VocabCard
+                            id = {keys[i]}
                             data = {this.props.VocabDict[keys[i]]}
                             size={this.props.size}
                             active={this.props.active}
+                            showDetail={this.props.showDetail}
         ></VocabCard>);
         }
     
@@ -107,12 +163,34 @@ class VocabGrid extends React.Component {
 
 class VocabCard extends React.Component {
     
+    state = {
+        
+        showDialog: false
+    }
+    
+    onHide = () => {
+        this.setState({showDialog: false});
+    }
+     
+    
+    handleClick = () => {
+        
+        this.setState({showDialog: true});
+        
+    }
+    
     render () {
         
         return (
             
+            <div>
+            <Modal centered show={this.state.showDialog} onHide={this.onHide}>
+    {this.props.data['w']}
+            </Modal>
             <Card
-            style={{height: "20rem", width: "15rem", marginRight: "1rem", marginLeft: "1rem", marginTop: "1rem"}}>
+            onClick={this.handleClick}
+            className="myvocabcard"
+            style={{height: "10rem", width: "15rem", marginRight: "1rem", marginLeft: "1rem", marginTop: "1rem"}}>
             
             <div className="cardHeader"
             style={{textAlign: "center",
@@ -121,11 +199,13 @@ class VocabCard extends React.Component {
             {this.props.data["w"]} <br></br>
 {this.props.active &&<ProgressBar 
                 now={this.props.data["s"]*10}
+                variant="success"
                 style={{marginTop: "1rem"}}/>}
             </div>
 
 
             </Card>
+</div>
         );
     }
 }
