b7761b6ab886fc016b5a41190bb1aff35198bdac
hilbert-spaess <jacobcoxon@hotmail.co.uk>
2020-09-17 10:42:03 +0000

    hi

--- src/LearningContainer.js
+++ src/LearningContainer.js
@@ -91,42 +91,101 @@ const LearningContainerData = (props) => {
     if (data.displayType == "newUser") {
         return <Redirect to="/newuser"/>;
     }
-    console.log(data.context);
+    console.log(data);
     return (
         <div>
-        <LearningContainer
-interaction={data.interaction}
-chunkid={data.chunkid}
-context={data.context}
-grammar={data.grammar}
-currentInteraction={data.currentInteraction}
-keyloc={data.keyloc}
-displayType={data.displayType}
-tryAgain={getTokenAndTryAgain}
-storeAnswer={props.storeAnswer}
-handleNext={handleNext}
-/>
+        <LearningContainerUpdatable
+            allChunks = {data.allChunks}
+        />          
 </div>
     );
 }
+
+class LearningContainerUpdatable extends React.Component {
+    
+    state = {
+        parcelData: {},
+        currentChunkNo: 0,
+        done: 0,
+        allChunks: this.props.allChunks
+    }
+    
+    handleNext = (parcelData) => {
+        console.log(parcelData)
+        this.setState({parcelData});
+        console.log("updatein");
+        if (this.state.currentChunkNo < this.props.allChunks.length - 1) {
+            var i = this.state.currentChunkNo;
+            this.setState({currentChunkNo: i + 1});
+        } else {
+            this.setState({done: 1});
+        }
+        console.log(this.state.currentChunkNo);
+    }
+    
+    render () {
+        
+        if (this.state.done == 0) {
+            
+            console.log(this.props.allChunks[this.state.currentChunkNo]);
+    
+        return (
+            <LearningContainerLogging
+                parcelData = {this.state.parcelData}
+                currentChunk = {this.props.allChunks[this.state.currentChunkNo]}
+                handleNext = {this.handleNext}
+            />
+        );
+    } else {
+        return (0);
+    }
+    }
+}   
+
+const LearningContainerLogging = (props) => {
+    
+    const payload = props.parcelData;
+    const {login, getAccessTokenWithPopup } = useAuth0();
+    const opts = {audience: APIHOST, 
+                  fetchOptions: {method: 'post',
+                                 body: payload,
+                                 headers: {'Access-Control-Allow-Credentials': 'true',
+                                           'Access-Control-Allow-Origin': '*',
+                                           'Accept': 'application/json',
+                                            'Content-Type': 'application/json',
+                                          'Access-Control-Request-Method': 'POST'}}};
+    const {error, loading, refresh} = useApi(APIHOST + '/api/getchunk', payload, opts);
+    
+    const handleNext = async (parcelData) => {
+        props.handleNext(parcelData);
+        refresh();
+    }
+    
+    console.log(props.currentChunk)
+
+    return (
+        <div>
+        <LearningContainer
+            currentChunk = {props.currentChunk}
+            handleNext = {props.handleNext}
+        />
+        </div>
+    );
+}
+
+    
+    
 
 class LearningContainer extends React.Component {
 
     state = {
         done: 0,
-        interaction: this.props.interaction,
-        stats: {},
         showDialog: false,
-        chunkid: this.props.chunkid,
         isLoading: 0,
-        context: this.props.context,
-        grammar: this.props.grammar,
-        userid: this.props.userid,
         answeredCorrect: "-1",
-        currentInteraction: this.props.currentInteraction,
-        displayType: this.props.displayType,
         answers: [],
-        key: this.props.keyloc
+        currentInteraction: 0,
+        limbo: false
         }
     
     storeAnswer = (correct) => {
@@ -134,39 +193,108 @@ class LearningContainer extends React.Component {
             answers: [...prevState.answers, correct]
         }));
     };
-    loadData = (data) => {
-        this.setState({stats: data});
+    
+    handleAnswer = (correct) => {
+        this.storeAnswer(correct);
+        if (correct) {
+            this.setState({done: 1,
+                   answeredCorrect: 1});
+        } else {
+            this.setState({done: 1,
+                   answeredCorrect: 0});
+        }
+        if (this.props.currentChunk["interaction"][this.state.currentInteraction]["key"] == 1) {
+            this.handleOpenDialog();
+        } else {
+            this.handleCloseDialog();
+        }
+            
+    }
+    
+    nextInteraction = () => {
+        const a = this.state.currentInteraction + 1;
+        this.setState({currentInteraction: a});
+    }
+    
+    handleOpenDialog = () => {
+        this.setState({showDialog: true});
+    }
+
+    handleCloseDialog = () => {
+        
+        this.setState({showDialog: 0,
+                      done: 0});
+        console.log("CURRENT INTERACTION");
+        console.log(this.state.currentInteraction);
+        if (Object.keys(this.props.currentChunk["interaction"]).length > this.state.currentInteraction+1) {
+            this.nextInteraction();
+        } else {
+            this.setState({limbo: true})
+        }
     }
     
     handleNext = (correct) => {
         this.props.handleNext({
-              answeredCorrect: correct,
-              interaction: this.state.interaction,
-              currentInteraction: this.state.currentInteraction,
-              chunkId: this.state.chunkid,
-              keyloc: this.props.keyloc,
-              answers: this.state.answers});
+        answeredCorrect: this.state.answeredCorrect,
+        chunkId: this.props.currentChunk["chunkid"],
+        keyloc: this.props.currentChunk["keyloc"],
+        answers: this.state.answers});
+        this.setState({currentInteraction: 0,
+                       answers: [],
+                       limbo: false});
     }
+
     
     render () {
+        const context = this.props.currentChunk["context"];
+        const interaction = this.props.currentChunk["interaction"];
+        const location = this.props.currentChunk["interaction"][this.state.currentInteraction]["location"];
+        const answer= this.props.currentChunk["context"][this.props.currentChunk["interaction"][this.state.currentInteraction]["location"]]['w'];
+        const interactionMode= this.props.currentChunk["interaction"][this.state.currentInteraction]["mode"];
+        const length = this.props.currentChunk["interaction"][this.state.currentInteraction]["length"];
+        console.log(interaction);
+        console.log(interactionMode);
         
-        console.log(this.state.displayType);
-        console.log(this.props.key);
         if (this.state.isLoading == 0 ) {
-            console.log(this.state.context);
                 return (
-                <div>
-                <LearningInstance
-            context={this.state.context}
-            interaction={this.state.interaction}
-            handleNext={this.handleNext}
-            storeAnswer={this.storeAnswer}
-            answers={this.state.answers}
-            />
-                </div>
-                );
+	    <Container fluid="lg">
+		<Modal centered show={this.state.showDialog} onHide={this.handleCloseDialog}>
+		<AnswerCard
+	    word={context[location]['vw']}
+	    answeredCorrect={this.state.answeredCorrect}
+	    handleHide={this.handleCloseDialog}
+	    specificInteraction={interaction[this.state.currentInteraction]}/>
+		    </Modal>
+		    <Row>
+		    <Col>
+		    <TextCard
+		context={context}
+		length={length}
+		interaction={interaction}
+		currentInteraction={this.state.currentInteraction}
+		answer={answer}
+        answers={this.state.answers}
+		location={location}
+		handleAnswer={this.handleAnswer}/>
+		    </Col>
+		    </Row>
+		    <Row>
+		    <Col>
+		    <InteractionCard
+		done={this.state.done}
+		answeredCorrect={this.state.answeredCorrect}
+		interactionMode={interactionMode}
+		interaction={interaction[this.state.currentInteraction]}
+		answer={answer}
+        limbo={this.state.limbo}
+        handleNext={this.handleNext}
+		    />
+		    </Col>
+		    </Row>
+		</Container>
+	);
         }
-        else if (this.state.displayType == "done") {
+        else if (this.props.displayType == "done") {
             return (
                 <div className = "ui centered card">
                 No more reviews.
@@ -178,116 +306,6 @@ class LearningContainer extends React.Component {
                 </div>
             );
         }
-    }
-};
-
-class LearningInstance extends React.Component {
-    
-    state = {
-        currentInteraction: 0,
-        location: this.props.interaction[0]["location"],
-        answer: this.props.context[this.props.interaction[0]["location"]]['w'],
-        interactionMode: this.props.interaction[0]["mode"],
-        length: this.props.interaction[0]["length"],
-        showDialog: 0,
-        done: 0,
-        limbo: false,
-        answeredCorrect: -1
-    }
-    
-    handleNext = (event) => {
-        this.props.handleNext(this.state.answeredCorrect);
-    }
-
-    nextInteraction = () => {
-        console.log("Loading the next interaction innit");
-        const a = this.state.currentInteraction + 1;
-        console.log(a);
-        this.setState({currentInteraction: a,
-                   location: this.props.interaction[a]["location"],
-                   answer: this.props.context[this.props.interaction[a]["location"]]['w'],
-                   interactionMode: this.props.interaction[a]["mode"],
-                   length: this.props.interaction[a]["length"],
-                });
-    }
-
-    handleCloseDialog = () => {
-        
-        this.setState({showDialog: 0});
-        console.log("CURRENT INTERACTION");
-        console.log(this.state.currentInteraction);
-        if (Object.keys(this.props.interaction).length > this.state.currentInteraction+1) {
-            this.setState({done: 0,
-                   answeredCorrect: -1});
-            this.nextInteraction();
-        } else {
-            this.setState({limbo: true})
-        }
-    }
-
-    handleOpenDialog = () => {
-        console.log("opening dialog");
-        this.setState({showDialog: 1})
-    }
-    
-    handleAnswer = (correct) => {
-        console.log("HENNNNLO");
-        this.props.storeAnswer(correct);
-        if (correct) {
-            this.setState({done: 1,
-                   answeredCorrect: 1});
-        } else {
-            this.setState({done: 1,
-                   answeredCorrect: 0});
-        }
-        console.log("HEMLO KECCHAN");
-        console.log(this.props.interaction);
-        if (this.props.interaction[this.state.currentInteraction]["key"] == 1) {
-            this.handleOpenDialog();
-        } else {
-            this.handleCloseDialog();
-        }
-            
-    }
-    
-    render () {
-	return (
-	    <Container fluid="lg">
-		<Modal centered show={this.state.showDialog} onHide={this.handleCloseDialog}>
-		<AnswerCard
-	    word={this.props.context[this.state.location]['vw']}
-	    answeredCorrect={this.state.answeredCorrect}
-	    handleHide={this.handleCloseDialog}
-	    specificInteraction={this.props.interaction[this.state.currentInteraction]}/>
-		    </Modal>
-		    <Row>
-		    <Col>
-		    <TextCard
-		context={this.props.context}
-		length={this.state.length}
-		interaction={this.props.interaction}
-		currentInteraction={this.state.currentInteraction}
-		answer={this.state.answer}
-        answers={this.props.answers}
-		location={this.state.location}
-		handleAnswer={this.handleAnswer}/>
-		    </Col>
-		    </Row>
-		    <Row>
-		    <Col>
-		    <InteractionCard
-		done={this.state.done}
-		answeredCorrect={this.state.answeredCorrect}
-		interactionMode={this.state.interactionMode}
-		interaction={this.props.interaction[this.state.currentInteraction]}
-		answer={this.state.answer}
-        limbo={this.state.limbo}
-        handleNext={this.handleNext}
-		    />
-		    </Col>
-		    </Row>
-		</Container>
-	);
     }
 }
 
@@ -302,6 +320,12 @@ class TextCard extends React.Component {
 
     componentDidMount() {
 	console.log("MOUNTING!");
+    }
+    
+    componentDidUpdate = (prevProps) => {
+        if (prevProps.context !== this.props.context) {
+            this.setState({value: this.props.context[this.props.location]['w'][0]});
+        }
     }
     
     handleChange = (event) => {
@@ -368,25 +392,17 @@ class Words extends React.Component {
 	    } else {
 		var tcolour = "black";
 	    }
-        console.log(context[i]['w'])
-        console.log(context[i]['w']==",")
-        console.log(',' in punct);
-        console.log(punct.length);
         if ((punct.includes(context[i]['w'])) || i == 0) {
-            console.log("PUNCT");
             var spc = "";
         } else {
-            console.log("NOPUNCT");
             var spc = " ";
         }
 	    
 	    if (!("i" in context[i])) {
 		words.push(<Text>{spc + context[i]['w']}</Text>);
 	    } else if (i != location) {
-            console.log("HEMLOE");
             console.log(this.props.answers[context[i]["i"]])
             if (this.props.answers[context[i]["i"]] == 1) {
-                console.log("HI");
                 words.push(<Text style={{color: "green"}}>{spc + context[i]['w']}</Text>);
             } else {
                 words.push(<Text style={{color: "red"}}>{spc + context[i]['w']}</Text>);
