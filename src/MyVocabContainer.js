05254a76af7d9047c32dcebb78f6ce1294dde8de
hilbert-spaess <jacobcoxon@hotmail.co.uk>
2020-10-14 20:13:24 +0000

    hi

--- src/MyVocabContainer.js
+++ src/MyVocabContainer.js
@@ -8,6 +8,7 @@ import {APIHOST} from './api_config.js';
 import {BarWrapped, FreeBarWrapped} from './sidebar.js';
 import {Text} from 'react-native';
 import {Redirect} from 'react-router-dom';
+import {PlusCircle} from 'react-feather';
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -66,17 +67,102 @@ export const LoadingVocabContainer = () => {
     }
     
     return (
-         <MyVocabContainer1
+         <MyVocabContainerSubmit
         data={data}/>
         );
 }   
+
+class MyVocabContainerSubmit extends React.Component {
+    
+    state = {payload: null,
+             type: "null"}
+    
+    handleSubmit = (vocab) => {
+        
+        this.setState({payload: vocab,
+                       type: "submit"});
+        
+    }
+    
+    handleWipe = () => {
+        
+        this.setState({payload: null, type: "null"});
+        
+    }
+    
+    confirmSubmit = (data) => {
+        
+        this.setState({payload: data, type: "confirm"});
+        
+    }
+    
+    render () {
+        
+        return (
+            
+            <MyVocabContainerLogging
+            payload={this.state.payload}
+            type={this.state.type}
+            data={this.props.data}
+            handleSubmit={this.handleSubmit}
+            handleWipe={this.handleWipe}
+            confirmSubmit={this.confirmSubmit}/>
+    );
+    }
+}
+
+const MyVocabContainerLogging = (props) => {
+    
+    const payload = {payload: props.payload, type: props.type};
+    const {login, getAccessTokenWithPopup } = useAuth0();
+    const opts = {audience: APIHOST, 
+                  fetchOptions: {method: 'post',
+                                 body: payload,
+                                 headers: {'Access-Control-Allow-Credentials': 'true',
+                                           'Access-Control-Allow-Origin': '*',
+                                           'Accept': 'application/json',
+                                            'Content-Type': 'application/json',
+                                          'Access-Control-Request-Method': 'POST'}}};
+    const {error, loading, data, refresh} = useApi(APIHOST + '/api/newvocabword', payload, opts);
+    
+    const handleSubmit = async (vocab) => {
+        props.handleSubmit(vocab);
+        console.log("WOOOOOOOW");
+        console.log("WOOOOOOOW");
+        refresh();
+    }
+    
+    const wipeSubmit = async () => {
+        props.handleWipe();
+        console.log("WOOOOOOOW");
+        console.log("WOOOOOOOW");
+        refresh();
+    }
+    
+    const confirmSubmit = async (data) => {
+        props.confirmSubmit(data);
+        refresh();
+    }
+    
+    return (
+        <MyVocabContainer1
+        data={props.data}
+        handleSubmit={handleSubmit}
+        submitdata={data}
+        wipeSubmitData={wipeSubmit}
+        confirmSubmit={confirmSubmit}/>
+            );
+}
+    
 
 class MyVocabContainer1 extends React.Component {
     
     state = {
         showDialog: false,
+        showAddNew: false,
         detailId: null,
-        detailActive: null
+        detailActive: null,
+        vocab: this.props.data.vocab
     }
     
     showDetail = (id, active) => {
@@ -95,20 +181,71 @@ class MyVocabContainer1 extends React.Component {
         
     }
     
+    addVocab = () => {
+        
+        this.setState({showAddNew: true});
+        console.log("Add vocab you dumbo");
+        
+    }
+    
+    hideShowNew = () => {
+        this.setState({showAddNew: false});
+        this.props.wipeSubmitData();
+    }
+    
+    handleNewVocabSubmit = (vocab) => {
+        console.log(vocab);
+        this.props.handleSubmit(vocab);
+    }
+    
+    confirmSubmit = (data) => {
+        
+        this.props.confirmSubmit(data);
+        this.setState({showAddNew: false});
+        
+        var newvocab = this.state.vocab;
+        newvocab.push({"w": data[0], "l": this.props.data.maxlevel});
+        this.setState({vocab: newvocab});
+        
+    }
+    
     render () {
+        
+        var words = [];
+        
+        for (var i = 0; i <this.state.vocab.length; i++) {
+            
+            words.push(this.state.vocab[i]["w"]);
+                       
+        }
+        
+        
         
         return (
             <div>
-            <div style={{marginTop: "1em", textAlign: "center", fontSize: "50px"}}>My Vocab</div>
+            <div style={{marginTop: "1em", fontSize: "50px", textAlign: "center"}}>
+ <PlusCircle size={50} onClick={this.addVocab} style={{cursor: "pointer", marginRight: "3em", color: "green"}}/>    My Vocab <span style={{color: "white"}}> <PlusCircle size= {50} style={{marginLeft: "3em"}}/> </span>
+            </div>
             <Modal centered show={this.state.showDialog} onHide={this.onHide}>
             {this.state.detailActive && this.props.data.active[this.state.detailId]['w']}
     </Modal>
+            <Modal centered show={this.state.showAddNew} onHide={this.hideShowNew}>
+                <FindNew
+                 submitdata={this.props.submitdata}
+                 newchoices={this.props.data.choices}
+                 handleSubmit={this.handleNewVocabSubmit}
+                 words={words}
+                 handleHide={this.hideShowNew}
+                 wipeSubmit={this.props.wipeSubmitData}
+                 confirmSubmit={this.confirmSubmit}/>
+                </Modal>
         <div style={{marginTop: "1em"}}>
 <VocabGrid
-VocabDict={this.props.data.vocab}
+VocabDict={this.state.vocab}
 showDetail={this.props.showDetail}
 userlevel={this.props.data.level}
 size="2em"/></div>
+<div style={{marginBottom: "3em"}}/>
                     </div>
     );
 
@@ -365,6 +502,171 @@ class FutureVocabCard extends React.Component {
         );
     }
 }
+                  
+        
+
+class FindNew extends React.Component {
+    
+    state = {warning: "",
+             inputting: true,
+             loading: false}
+    
+    handleChange = (event) => {
+        
+        if (this.state.warning != "" || ((this.props.submitdata != null) && (this.props.submitdata["state"] == "not_in_dictionary"))) {
+            
+            this.setState({warning: ""});
+            this.props.wipeSubmit();
+            this.setState({inputting: true});
+            
+        }
+        
+        if (this.state.inputting == true) {
+        
+            event.preventDefault();
+            this.setState({value: event.target.value});
+        }
+    }
+    
+    handleSubmit = (event) => {
+        
+        event.preventDefault();
+        
+        if (this.state.inputting == true) {
+            
+            console.log(this.props.words);
+            
+            if (this.props.words.includes(this.state.value)) {
+                
+                this.setState({warning: "Already in vocab!"});
+                
+            }
+            
+            else {
+        
+                this.setState({loading: true,
+                               inputting: false});
+                console.log("Word submit");
+                this.props.handleSubmit(this.state.value);
+            }
+        }
+    }
+    
+    
+    render () {
+        
+        console.log("SUBMIT DATA");
+        if ((this.props.submitdata != null) && (this.props.submitdata["state"] == "in_dictionary") && (this.props.submitdata["data"][0][0] == this.state.value)) {
+            var choosing = true;
+            console.log("CHOOSINGTRUE");
+        } else {
+            var choosing = false;
+            console.log("CHOOSINGFALSE");
+        }
+        
+        if ((this.props.submitdata != null) && (this.props.submitdata["state"] == "not_in_dictionary") && (this.props.submitdata["data"] == this.state.value)) {
+            
+            console.log("CHANGING WARNING");
+            
+            var warning = "Not in dictionary.";
+            
+            
+        } else {
+            
+            var warning = "";
+            
+        }
+        
+        return (
+            
+            <Card className="findnewcard">
+            <div style={{marginTop: "0.5em"}}>
+            {!choosing && "Add a new word"}
+{choosing && "Confirm new word:"}
+            </div>
+           
+            <div style={{color: "red", marginTop: "0.5em"}}>{this.state.warning}{warning}</div>
+            <div>{!choosing && <NewInput 
+                              handleSubmit={this.handleSubmit}
+                              handleChange={this.handleChange}
+                              value={this.state.value}/>}</div>
+            <div>{choosing && <Choose data={this.props.submitdata["data"]} 
+                                      handleCancel={this.props.handleHide}
+                                      confirmSubmit={this.props.confirmSubmit}/>}</div>
+             
+            </Card>
+            
+        );
+    }
+}
+
+class NewInput extends React.Component {
+    
+    render () {
+        
+        return (
+            <>
+             <form className="commentForm" onSubmit={this.props.handleSubmit}>
+                <input type="text" style={{fontSize: "2em",  border: "none", outline: "none", borderBottom: "1px dashed black", textAlign: "center", width: "80%"}} value={this.props.value} onChange={this.props.handleChange}/>
+                    </form>
+            <div style={{textAlign: "center"}}>
+            <button className="newvocabsubmit" onClick={this.props.handleSubmit} style={{width: "50%"}}>Add word</button>
+            </div>
+            </>
+        );
+    }
+}
+        
+class Choose extends React.Component {
+    
+    render () {
+        
+        var options = []
+        
+        for (var i = 0; i < this.props.data.length; i++ ) {
+            console.log(i);
+            options.push(<Option confirmSubmit={this.props.confirmSubmit} data={this.props.data[i]}/>);
+    
+        }
+    
+        console.log(options);
+        
+        return (
+            <>
+            <div>{options}</div>
+            <button className="cancelbutton" onClick={this.props.handleCancel} style={{width: "30%", marginBottom: "1em"}}>Cancel</button>
+            </>
+        );
+    }
+}
+
+class Option extends React.Component {
+    
+    handleClick = () => {
+        
+        this.props.confirmSubmit(this.props.data);
+        
+    }
+    
+    render () {
+        
+        console.log(this.props.data);
+        
+        return (
+            <Card className="newvocaboptioncard" onClick = {this.handleClick} style={{cursor: "pointer", borderColor: "lightgrey", marginBottom: "1em", marginLeft: "10%", marginRight: "10%"}}>
+            
+            <div> <span style={{fontSize: "30px", marginRight: "1em"}}>{this.props.data[0] + "       "}</span><span style={{fontSize: "20px", color: "grey"}}> {this.props.data[2]}</span></div>
+
+            </Card>
+
+        );
+    }
+}
+            
+            
+            
+
+
 
     
             
