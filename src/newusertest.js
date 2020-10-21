f22a8e2afbc8a55a65e90884e833e74bb50a0081
hilbert-spaess <jacobcoxon@hotmail.co.uk>
2020-10-21 15:22:14 +0000

    hi

--- src/newusertest.js
+++ src/newusertest.js
@@ -217,7 +217,7 @@ export class CourseChoice1 extends React.Component {
         
         for (var i = 0; i < keys.length; i++) {
             
-            if (keys[i] == "1" || keys[i] == "2" || keys[i] == "5" || keys[i] == "7") {
+            if (keys[i] == "1" || keys[i] == "2" || keys[i] == "5" || keys[i] == "6" || keys[i] == "7") {
             
             
             choices.push(<CourseCard
