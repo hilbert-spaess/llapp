de819714db7f6d0bec8d1abf1541874a91193107
hilbert-spaess <jacobcoxon@hotmail.co.uk>
2020-10-24 15:20:39 +0000

    hi

--- src/register.js
+++ src/register.js
@@ -2,10 +2,25 @@ import {parse} from 'query-string';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Row, Card} from 'react-bootstrap';
+import {Redirect} from 'react-router-dom';
 
 export class Register extends React.Component {
     
+    state = {continued: null}
+    
+    handleSubmit = () => {
+        
+        this.setState({continued: 1});
+        
+    }
+    
     render () {
+        
+        if (this.state.continued == 1) {
+            
+            return <Redirect to="/home"/>
+                
+        }
         
         var params = parse(this.props.location.search);
         
@@ -16,9 +31,12 @@ export class Register extends React.Component {
             <header className="loginmasthead text-center text-white">
              <Row style={{justifyContent: "space-around"}}>
     <Card className="login-content">
-        
-        <div style={{color: "black", textAlign: "center", fontSize: "30px"}}>
+         
+        <div style={{color: "black", textAlign: "center", fontSize: "25px"}}>
             {message}
+</div>
+<div style={{textAlign: "center"}}>
+<button onClick={this.handleSubmit} style={{width: "40%", marginTop: "1em"}} className="btn btn-primary login-rounded-pill">Continue</button>
 </div>
             </Card>
             </Row>
